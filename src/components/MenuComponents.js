b906e1847311720a76559457992a649896c0394d
AbhiC7721 <abhic7721@gmail.com>
2020-09-07 22:05:36 +0530

    some event logging

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -10,10 +10,15 @@ class Menu extends Component {
         this.state = {
             selectedDish: null
         }
+        console.log('Menu Component constructor is invoked');
     }
 
     onDishSelect(dish) {
         this.setState({ selectedDish: dish});
+    }
+
+    componentDidMount() {
+        console.log('Menu Component componentDidMount is invoked');
     }
 
     renderDish(dish) {
@@ -50,12 +55,15 @@ class Menu extends Component {
             );
         });
 
+        console.log('Menu Component render is invoked');        
+
         return (
             <div className="container">
                 <div className="row">
                         {menu}
                 </div>
                 <div className="row">
+
                         {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
