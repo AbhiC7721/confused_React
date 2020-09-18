6407d752356104da3940a405301ba7bc0bad6b0e
AbhiC7721 <abhic7721@gmail.com>
2020-09-18 21:22:19 +0530

    React Redux Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,7 +3,13 @@ import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row} from 'reactstrap';
 import { Link } from  'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
- class Contact extends Component {
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
+class Contact extends Component {
 
     constructor(props){
         super(props);  
@@ -66,7 +72,21 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                                 <Col md={10}>
                                     <Control.text model=".firstname" id="firstname" name="firstname" 
                                         placeholder="First Name"
-                                        className="form-control" />   
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    /> 
+                                    <Errors
+                                        className="text-danger"
+                                        model=".firstname"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must ber greater than 2 cahracters',
+                                            maxLength: 'Must be 15 cahracters or less'
+                                        }} 
+                                    />      
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -74,7 +94,21 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                                 <Col md={10}>
                                     <Control.text model=".lastname" id="lastname" name="lastname" 
                                         placeholder="Last Name"
-                                        className="form-control" />   
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".lastname"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must ber greater than 2 cahracters',
+                                            maxLength: 'Must be 15 cahracters or less'
+                                        }} 
+                                    />       
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -82,7 +116,22 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                                 <Col md={10}>
                                     <Control.text model=".telnum" id="telnum" name="telnum" 
                                         placeholder="Tel. Number"
-                                        className="form-control" />    
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15), isNumber
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".telnum"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must ber greater than 2 numbers',
+                                            maxLength: 'Must be 15 numbers or less',
+                                            isNumber: 'Must be a number'
+                                        }} 
+                                    />        
                                 </Col>
                             </Row> 
                             <Row className="form-group">
@@ -90,7 +139,20 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                                 <Col md={10}>
                                     <Control.text model=".email" id="email" name="email" 
                                         placeholder="Email"
-                                        className="form-control" />
+                                        className="form-control"
+                                        validators={{
+                                            required, validEmail
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".email"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            validEmail: 'Invalid Email Address'
+                                        }} 
+                                    />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -104,7 +166,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
                                     </div>
                                 </Col>
                                 <Col md={{size: 3, offset: 1}}>
-                                    <Control.select model=".contactType" name="contactType" calssName="form-control">
+                                    <Control.select model=".contactType" name="contactType" className="form-control">
                                         <option>Tel.</option> 
                                         <option>Email</option>   
                                     </Control.select>
