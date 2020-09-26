9571d24d366e3a25a0daedc20e31c53d62aaebc0
AbhiC7721 <abhic7721@gmail.com>
2020-09-26 12:25:13 +0530

    Redux Thunk

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -38,7 +38,7 @@ class Header extends Component {
     render() {
         return(
             <React.Fragment>
-                <Navbar dark expand="md">
+                <Navbar dark expand="md" className="sticky-top">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
                         <NavbarBrand className="mr-auto" href="/">
